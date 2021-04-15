64b013767615a6f404e83f655ab42c5de4c10f82
pmh <pauline.miele@gmail.com>
2021-04-15 11:10:46 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Dashboard from './components/Dashboard'
+import Assets from './components/Assets'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li><Link to="/">Dashboard</Link></li>
+            <li><Link to="/assets">Assets</Link></li>
+          </ul>
+        </div>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/assets" component={Assets} />
+      </BrowserRouter>
+    </div >
   );
 }
 
